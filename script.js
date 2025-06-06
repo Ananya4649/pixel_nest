b6cb9569f106decf8ce0a4052ea7c93527697d7b
Ananya4649 <sgs.ananya.11sep.10d@gmail.com>
2025-06-06 17:03:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a, footer a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
